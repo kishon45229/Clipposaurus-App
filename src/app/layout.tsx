99b875deec7f8f4c6872222799dcd8f5f8c80113
kishon45229 <kishon45229@outlook.com>
2026-01-12 13:13:24 +0530

    Update OfflineAlert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ import { Funnel_Sans } from "next/font/google";
 import TargetCursor from "@/components/TargetCursor";
 import { OfflineAlert } from "@/components/custom/OfflineAlert";
 import { ThemeProvider } from "@/components/theme-provider";
-import { BetaNotice } from "@/components/custom/BetaNotice";
 
 export { siteMetadata as metadata };
 
@@ -48,7 +47,6 @@ export default function RootLayout({
               hideDefaultCursor={true}
             />
             <OfflineAlert />
-            <BetaNotice />
             <div className="flex flex-col min-h-screen md:portrait:min-h-screen md:portrait:justify-center md:portrait:py-10 md:portrait:gap-8 px-1 xs-min:px-2 sm:portrait:px-4 md:portrait:px-8 sm:landscape:px-4 md:landscape:px-6 bg-zinc-50 dark:bg-zinc-950">
               <div className="sticky top-0 z-10 portrait:h-[14dvh] xs-min:portrait:h-[8dvh] landscape:h-[16dvh] sm:landscape:h-[12dvh] lg:landscape:h-[8dvh] flex-shrink-0 w-full bg-zinc-50/80 dark:bg-zinc-950/80 backdrop-blur-sm border-b border-zinc-200 dark:border-zinc-800 md:portrait:static md:portrait:h-auto md:portrait:bg-transparent md:portrait:backdrop-blur-0 md:portrait:border-b-0">
                 <Navbar />
