119591cc423c85b23addbe3ec4fffefa5e959825
kishon45229 <kishon45229@outlook.com>
2026-01-17 16:42:07 +0530

    Update ResponsiveDialogTitle text size

--- src/components/create-drop/mobile-menu/MobileMenuHeader.tsx
+++ src/components/create-drop/mobile-menu/MobileMenuHeader.tsx
@@ -12,7 +12,7 @@ export const MobileMenuHeader = () => {
 
   return (
     <div className="text-center space-y-[clamp(0.25rem,1vw,0.5rem)]">
-      <ResponsiveDialogTitle className="font-semibold tracking-tight text-[clamp(1rem,3.5vw,1.5rem)] text-emerald-600 dark:text-emerald-500">
+      <ResponsiveDialogTitle className="font-semibold tracking-tight text-[clamp(1.25rem,3.5vw,1.5rem)] text-emerald-600 dark:text-emerald-500">
         {title}
       </ResponsiveDialogTitle>
 
