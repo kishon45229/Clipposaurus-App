cbd90ae7538368db033e3645601d77deb7f77a6d
kishon45229 <kishon45229@outlook.com>
2026-01-19 17:16:16 +0530

    Removed root URL from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,19 +4,19 @@ import { APP_URL } from "@/lib/urls";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: APP_URL!,
+      url: `${APP_URL}/create-drop`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: `${APP_URL}/create-drop`,
+      url: `${APP_URL}/unlock-drop`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.9,
     },
     {
-      url: `${APP_URL}/unlock-drop`,
+      url: `${APP_URL}/open-drop`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
